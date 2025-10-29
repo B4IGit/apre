4763cea0c041fe0f9c2e29a942dce178166d168c
Devin Ledesma <dev.dfledesma@gmail.com>
2025-10-29 14:40:32 -0500

    Initial commit

---
+++ apre-server/src/routes/reports/sales/index.js
@@ -0,0 +1,97 @@
+/**
+ * Author: Professor Krasso
+ * Date: 8/14/24
+ * File: index.js
+ * Description: Apre sales report API for the sales reports
+ */
+
+"use strict";
+
+const express = require("express");
+const { mongo } = require("../../../utils/mongo");
+
+const router = express.Router();
+
+/**
+ * @description
+ *
+ * GET /regions
+ *
+ * Fetches a list of distinct sales regions.
+ *
+ * Example:
+ * fetch('/regions')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get("/regions", (req, res, next) => {
+  try {
+    mongo(async (db) => {
+      const regions = await db.collection("sales").distinct("region");
+      res.send(regions);
+    }, next);
+  } catch (err) {
+    console.error("Error getting regions: ", err);
+    next(err);
+  }
+});
+
+/**
+ * @description
+ *
+ * GET /regions/:region
+ *
+ * Fetches sales data for a specific region, grouped by salesperson.
+ *
+ * Example:
+ * fetch('/regions/north')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get("/regions/:region", (req, res, next) => {
+  try {
+    mongo(async (db) => {
+      const salesReportByRegion = await db
+        .collection("sales")
+        .aggregate([
+          { $match: { region: req.params.region } },
+          {
+            $group: {
+              _id: "$salesperson",
+              totalSales: { $sum: "$amount" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              salesperson: "$_id",
+              totalSales: 1,
+            },
+          },
+          {
+            $sort: { salesperson: 1 },
+          },
+        ])
+        .toArray();
+      res.send(salesReportByRegion);
+    }, next);
+  } catch (err) {
+    console.error("Error getting sales data for region: ", err);
+    next(err);
+  }
+});
+
+// Follow along in class
+LOCALHOST: 3000 / api / reports / sales / sales - by - month / april;
+LOCALHOST: 3000 / api / reports / sales / sales - by - month / foo;
+LOCALHOST: 3000 / api / reports / sales / sales - by - month / bar;
+
+router.get("", (req, res, next) => {
+  try {
+  } catch (err) {
+    console.error("err", err);
+    next(err);
+  }
+});
+
+module.exports = router;
