f10e0c9f17378625261b59214b7e8abb3d131284
Devin Ledesma <dev.dfledesma@gmail.com>
2025-11-16 16:42:07 -0600

    Added new APIs for `customer-feedback-by-product` and refactored `channel-rating-by-month` route for formatting and consistency

--- apre-server/src/routes/reports/customer-feedback/index.js
+++ apre-server/src/routes/reports/customer-feedback/index.js
@@ -5,11 +5,11 @@
  * Description: Apre customer feedback API for the customer feedback reports
  */
 
-'use strict';
+"use strict";
 
-const express = require('express');
-const { mongo } = require('../../../utils/mongo');
-const createError = require('http-errors');
+const express = require("express");
+const { mongo } = require("../../../utils/mongo");
+const createError = require("http-errors");
 
 const router = express.Router();
 
@@ -25,69 +25,125 @@ const router = express.Router();
  *  .then(response => response.json())
  *  .then(data => console.log(data));
  */
-router.get('/channel-rating-by-month', (req, res, next) => {
+router.get("/channel-rating-by-month", (req, res, next) => {
   try {
     const { month } = req.query;
 
     if (!month) {
-      return next(createError(400, 'month and channel are required'));
+      return next(createError(400, "month and channel are required"));
     }
 
-    mongo (async db => {
-      const data = await db.collection('customerFeedback').aggregate([
-        {
-          $addFields: {
-            date: { $toDate: '$date' }
-          }
-        },
-        {
-          $group: {
-            _id: {
-              channel: "$channel",
-              month: { $month: "$date" },
+    mongo(async (db) => {
+      const data = await db
+        .collection("customerFeedback")
+        .aggregate([
+          {
+            $addFields: {
+              date: { $toDate: "$date" },
             },
-            ratingAvg: { $avg: '$rating'}
-          }
-        },
-        {
-          $match: {
-            '_id.month': Number(month)
-          }
-        },
-        {
-          $group: {
-            _id: '$_id.channel',
-            ratingAvg: { $push: '$ratingAvg' }
-          }
-        },
-        {
-          $project: {
-            _id: 0,
-            channel: '$_id',
-            ratingAvg: 1
-          }
-        },
-        {
-          $group: {
-            _id: null,
-            channels: { $push: '$channel' },
-            ratingAvg: { $push: '$ratingAvg' }
-          }
-        },
-        {
-          $project: {
-            _id: 0,
-            channels: 1,
-            ratingAvg: 1
-          }
-        }
-      ]).toArray();
+          },
+          {
+            $group: {
+              _id: {
+                channel: "$channel",
+                month: { $month: "$date" },
+              },
+              ratingAvg: { $avg: "$rating" },
+            },
+          },
+          {
+            $match: {
+              "_id.month": Number(month),
+            },
+          },
+          {
+            $group: {
+              _id: "$_id.channel",
+              ratingAvg: { $push: "$ratingAvg" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              channel: "$_id",
+              ratingAvg: 1,
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              channels: { $push: "$channel" },
+              ratingAvg: { $push: "$ratingAvg" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              channels: 1,
+              ratingAvg: 1,
+            },
+          },
+        ])
+        .toArray();
 
       res.send(data);
     }, next);
-
   } catch (err) {
-    console.error('Error in /rating-by-date-range-and-channel', err);
+    console.error("Error in /rating-by-date-range-and-channel", err);
+    next(err);
+  }
+});
+
+// API for customer-feedback-by-product
+router.get("/customer-feedback-by-product", (req, res, next) => {
+  try {
+    mongo(async (db) => {
+      const products = await db
+        .collection("customerFeedback")
+        .distinct("product");
+      res.send(products);
+    }, next);
+  } catch (err) {
+    console.error("Error getting products: ", err);
+    next(err);
+  }
+});
+
+// API for customer-feedback-by-product/:product
+router.get("/customer-feedback-by-product/:product", (req, res, next) => {
+  const product = req.params.product;
+
+  try {
+    mongo(async (db) => {
+      const customerFeedbackByProduct = await db
+        .collection("customerFeedback")
+        .aggregate([
+          { $match: { product } },
+          {
+            $group: {
+              _id: {
+                customer: "$customer",
+                product: "$product",
+                region: "$region",
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              customer: "$_id.customer",
+              product: "$_id.product",
+              region: "$_id.region",
+            },
+          },
+          { $sort: { customer: 1, product: 1 } },
+        ])
+        .toArray();
+
+      res.send(customerFeedbackByProduct);
+    }, next);
+  } catch (err) {
+    console.error("Error getting customer feedback data by product:", err);
     next(err);
   }
 });
